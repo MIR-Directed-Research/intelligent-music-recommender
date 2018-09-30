5cfb637f378bfff78c199d80238ea4e1a1c5fe88
John Verwolf <jdverwolf@gmail.com>
2018-09-29 21:48:45 -0700

    Add preliminary nlp

--- knowledge_base_api.py
+++ knowledge_base_api.py
@@ -3,3 +3,12 @@ This layer stores the interface to the knowledge-engine.
 It is primarily used by the query-engine and knowledge-generation
 components.
 """
+
+# TODO: Implement.
+def get_all_nouns():
+    """Gets a list of all the names, genres,
+    artists, ect. in the DB
+
+    :return: A list of all nouns in the database
+    """
+    return ['The Who']
--- nlp_layer.py
+++ nlp_layer.py
@@ -1,4 +1,81 @@
-"""
-This layer stores the NLP specific tooling and logic.
-It primarily interacts with the query engine.
-"""
+import re
+
+import nltk
+from nltk.corpus import stopwords
+
+from knowledge_base_api import get_all_nouns
+
+
+class NLP:
+    """
+    This layer stores the NLP specific tooling and logic.
+    It primarily interacts with the query engine.
+
+    Example sentences:
+        "Play Desposito"
+        "Play some jazz music"
+        "Play some faster music"
+        "Who is the the saxophone player?
+        "Play her top songs"
+
+    References:
+        https://stackoverflow.com/questions/42322902/how-to-get-parse-tree-using-python-nltk
+        https://www.nltk.org/book/ch08.html
+    """
+
+    keywords = {
+        'control_play': ['start', 'play'],
+        'query_artist': ['who', 'artist'],
+    }
+
+    def __init__(self):
+        # Download the stopwords if necessary.
+        try:
+            nltk.data.find('corpora/stopwords')
+        except LookupError:
+            nltk.download('stopwords')
+
+        # This is expensive, so only done on init
+        self.db_nouns_patterns = get_all_nouns()
+
+    def _get_stop_words(self):
+        # Remove all keywords from stopwords
+        stop_words = set(stopwords.words('english'))
+        for _, words in NLP.keywords.items():
+            for word in words:
+                try:
+                    stop_words.remove(word)
+                except:
+                    pass
+        return stop_words
+
+    def _gen_patterns(self):
+        # Generate RegEx patterns from keywords
+        patterns = {}
+        for intent, keys in NLP.keywords.items():
+            patterns[intent] = re.compile('|'.join(keys))
+        return patterns
+
+    def parse_input(self, msg):
+        # Identify the subject
+        subject = ''
+        for noun in self.db_nouns_patterns:
+            if noun.lower() in msg:
+                msg = msg.replace(noun.lower(), '')
+                subject = noun.strip()
+                break
+
+        # Clean the stopwords from the input.
+        stop_words = self._get_stop_words()
+        clean_msg = ' '.join([word for word in msg.lower().split(' ')
+                              if word not in stop_words])
+
+        # Parse the keywords from the filtered input.
+        patterns = self._gen_patterns()
+        intents = []
+        for intent, pattern in patterns.items():
+            sub_msg = re.sub(pattern, '', clean_msg)
+            if sub_msg != clean_msg:
+                intents.append(intent)
+                clean_msg = sub_msg
+        return intents, subject or clean_msg.strip()
