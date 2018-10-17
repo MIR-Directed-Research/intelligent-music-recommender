d7144af2fa959c2d8f5021f5fb2dc28fa673b993
John Verwolf <jdverwolf@gmail.com>
2018-10-17 11:56:57 -0700

    Update KB to get all subjects from DB

--- tests/test_nlp_layer.py
+++ tests/test_nlp_layer.py
@@ -1,12 +1,19 @@
 import unittest
 from unittest.mock import MagicMock
+
+import os
+
 from nlp.nlp_layer import NLP
 from knowledge_base.api import KnowledgeBaseAPI
 
 
 class TestNLP(unittest.TestCase):
     def setUp(self):
-        self.nlp = NLP()
+        self.DB_path = "tests/test.db"
+        # Check working directory, update path to DB accordingly.
+        if os.getcwd().split('/')[-1] == 'tests':
+            self.DB_path = "test.db"
+        self.nlp = NLP(self.DB_path)
 
     def test_parse_input_play(self):
         """Test that `control_play` intention is parsed."""
@@ -20,11 +27,12 @@ class TestNLP(unittest.TestCase):
         self.assertEqual(str(output[1]), '')
 
     def test_parse_input_KB_API(self):
-        KnowledgeBaseAPI.get_all_nouns = MagicMock(return_value=['The Who'])
-        self.nlp = NLP()  # Re-instantiate nlp so it uses the mock value.
+        KnowledgeBaseAPI.get_all_music_entities = MagicMock(return_value=['The Who'])
+        self.nlp = NLP(self.DB_path)  # Re-instantiate nlp so it uses the mock value.
         output = self.nlp.parse_input('play the who')
         self.assertEqual(str(output[0]), "['control_play']")
         self.assertEqual(str(output[1]), 'The Who')
+
 
 if __name__ == '__main__':
     unittest.main()
