a3c6424be75d6e477adea9062ef2e7fc98973666
John Verwolf <jdverwolf@gmail.com>
2018-10-22 07:09:50 -0700

    Update controller to use interactions class

--- play_controller/controller.py
+++ play_controller/controller.py
@@ -2,24 +2,25 @@ import sys
 
 from knowledge_base.api import KnowledgeBaseAPI
 from nlp.nlp_layer import NLP
-from play_controller import commands
+from play_controller.interactions import Interactions
 
 
 class Controller:
     def __init__(self, db_path):
         self.DB_path = db_path
-        self.nlp = NLP(self.DB_path, commands.keywords)
         self.kb_api = KnowledgeBaseAPI(self.DB_path)
+        self.interactions = Interactions(self.DB_path)
+        self.nlp = NLP(self.DB_path, self.interactions.keywords)
 
     def __call__(self, raw_input):
-        command_lst, db_subject, remaining_words = self.nlp(raw_input)
+        command_lst, db_subject = self.nlp(raw_input)
+        actions = self.interactions.actions
         results = []
         for command in command_lst:
-            func = commands.actions.get(command, commands.default)
-            print('Entity:\t\t{}'.format(db_subject if db_subject else 'NONE'), file=sys.stdout)
-            print('Command:\t{}'.format(command if command else 'NONE'), file=sys.stdout)
+            func = actions.get(command, self.interactions.default)
+            print('Matching DB Entity:\t{}'.format(db_subject if db_subject else 'NONE'), file=sys.stderr)
+            print('Command:\t\t\t{}'.format(command if command else 'NONE'), file=sys.stderr)
             results.append(func(
                 db_subject=db_subject,
-                remaining_words=remaining_words
             ))
         return results
