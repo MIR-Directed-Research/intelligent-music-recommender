3c04bb858d5db7f4e6138cc023d5ea297ea4be75
John Verwolf <jdverwolf@gmail.com>
2018-10-22 07:14:38 -0700

    Add regression test for nlp bug

--- tests/test_nlp_layer.py
+++ tests/test_nlp_layer.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 
 from knowledge_base.api import KnowledgeBaseAPI
 from nlp.nlp_layer import NLP
-from play_controller.commands import intentions
+from play_controller.interactions import Interactions
 
 
 class TestNLP(unittest.TestCase):
@@ -13,9 +13,11 @@ class TestNLP(unittest.TestCase):
         # Check working directory, update path to DB accordingly.
         if os.getcwd().split('/')[-1] == 'tests':
             self.DB_path = "test.db"
-        self.keywords = {k: v[0] for k, v in intentions.items()}
-        self.nlp = NLP(db_path=self.DB_path,
-                       keywords=self.keywords)
+
+        self.kb_api = KnowledgeBaseAPI(self.DB_path)
+        self.interactions = Interactions(self.DB_path)
+        self.nlp = NLP(self.DB_path, self.interactions.keywords)
+        self.keywords = self.interactions.keywords
 
     def test_parse_input_play(self):
         """Test that `control_play` intention is parsed."""
@@ -23,7 +25,6 @@ class TestNLP(unittest.TestCase):
         self.assertEqual(str(output[0]), "['control_play']")
         self.assertEqual(str(output[1]), '')
         self.assertEqual(str(output[2]), 'clash')
-
 
     def test_parse_input_query(self):
         output = self.nlp('Who is this artist')
@@ -38,6 +39,11 @@ class TestNLP(unittest.TestCase):
         self.assertEqual(str(output[1]), 'The Who')
         self.assertEqual(str(output[2]), '')
 
+    def test_call_functional_test(self):
+        output = self.nlp('play Justin bieber')
+        self.assertEqual(str(output[0]), "['control_play']")
+        self.assertEqual(str(output[1]), 'Justin Bieber')
+        self.assertEqual(str(output[2]), '')
 
 
 if __name__ == '__main__':
