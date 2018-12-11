d953b604540670a62ab9ae2f779acc8c82020fcc
John Verwolf <jdverwolf@gmail.com>
2018-12-10 21:39:31 -0800

    Add test for tree parser

--- run_tests.py
+++ run_tests.py
@@ -9,7 +9,8 @@ To run the tests:
 """
 from tests.test_nlp_layer import TestNLP
 from tests.test_knowledge_base_api import TestMusicKnowledgeBaseAPI
-from tests.test_system_entry import TestSystemEntry
+from tests.test_system_entry_bag_of_words import TestSystemEntryBOW
+from tests.test_system_entry_tree_parser import TestSystemEntryTreeParser
 from tests.test_db_schema import TestDbSchema
 
 if __name__ == '__main__':
